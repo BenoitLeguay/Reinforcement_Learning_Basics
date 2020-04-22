ffdb43d6aff89496e19d51861068a810ef464333
¨BenoitLeguay¨ <benoit.leguay@gmail.com>
2020-04-22 18:24:36 +0200

    changing repo tree archi

--- function_approximation/mountain_car/agent.py
+++ function_approximation/mountain_car/agent.py
@@ -1,6 +1,7 @@
-import gym
 import numpy as np
 import tiles3 as tc
+#https://www.aaai.org/ocs/index.php/AAAI/AAAI12/paper/view/5092/5494
+#http://incompleteideas.net/book/first/ebook/node89.html
 
 
 class TileCoder:
@@ -31,6 +32,8 @@ class Agent:
         self.num_action = agent_init["num_action"]
         self.w = np.zeros((agent_init["num_action"], agent_init["tile_coder"]["hash_size"]))
         self.tile_coder = TileCoder(*agent_init["tile_coder"].values())
+        self.max_position = agent_init["max_position_init"]
+        self.max_position_reward_bonus = agent_init["max_position_reward_bonus"]
 
     def e_greedy(self, active_tiles):
 
@@ -45,14 +48,24 @@ class Agent:
 
         return action
 
+    def max_position_reward_function(self, new_position, reward):
+        if new_position > self.max_position:
+            self.max_position = new_position
+            reward += self.max_position_reward_bonus
+
+        return reward
+
     def choose_action(self, state):
         pass
 
     def update(self, state, reward, done):
+        next_action = -1
         if not done:
-            self.update_step(state, reward)
+            next_action = self.update_step(state, reward)
         if done:
             self.update_end(state, reward)
+
+        return next_action
 
     def update_step(self, state, reward):
         pass
@@ -68,7 +81,7 @@ class SarsaAgent(Agent):
         self.next_tiles = None
         self.next_action = None
 
-    def choose_action(self, state):
+    def episode_init(self, state):
 
         active_tiles = self.tile_coder.get_active_tiles(state)
         action = self.e_greedy(active_tiles)
@@ -79,15 +92,20 @@ class SarsaAgent(Agent):
         return action
 
     def update_step(self, next_state, reward):
-
         current_action = self.next_action
         current_tiles = self.next_tiles
 
-        self.choose_action(next_state)
+        next_tiles = self.tile_coder.get_active_tiles(next_state)
+        next_action = self.e_greedy(next_tiles)
 
-        target = reward + self.discount_factor * np.sum(self.w[self.next_action, self.next_tiles]) \
+        target = reward + self.discount_factor * np.sum(self.w[next_action, next_tiles]) \
                  - np.sum(self.w[current_action, current_tiles])
         self.w[current_action, current_tiles] += self.learning_rate * target
+
+        self.next_action = next_action
+        self.next_tiles = next_tiles
+
+        return next_action
 
     def update_end(self, state, reward):
         current_action = self.next_action
@@ -101,74 +119,82 @@ class QLearningAgent(Agent):
 
     def __init__(self, agent_init):
         super().__init__(agent_init)
-        self.current_tiles = None
-        self.current_action = None
+        self.next_tiles = None
+        self.next_action = None
 
-    def choose_action(self, state):
+    def episode_init(self, state):
 
-        if self.current_tiles is None:
-            active_tiles = self.tile_coder.get_active_tiles(state)
-        else:
-            active_tiles = self.current_tiles
-
+        active_tiles = self.tile_coder.get_active_tiles(state)
         action = self.e_greedy(active_tiles)
 
-        if self.current_tiles is None:
-            self.current_tiles = np.copy(active_tiles)
-
-        self.current_action = action
+        self.next_tiles = np.copy(active_tiles)
+        self.next_action = action
 
         return action
 
     def update_step(self, next_state, reward):
 
+        current_action = self.next_action
+        current_tiles = self.next_tiles
+
         next_active_tiles = self.tile_coder.get_active_tiles(next_state)
 
         target = reward + self.discount_factor * np.max(self.w[:, next_active_tiles], axis=0).sum() \
-                 - np.sum(self.w[self.current_action][self.current_tiles])
-        self.w[self.current_action][self.current_tiles] += self.learning_rate * target
+                 - np.sum(self.w[current_action][current_tiles])
+        self.w[current_action][current_tiles] += self.learning_rate * target
 
-        self.current_tiles = next_active_tiles
+        self.next_tiles = np.copy(next_active_tiles)
+        self.next_action = self.e_greedy(next_active_tiles)
+
+        return self.next_action
 
     def update_end(self, state, reward):
-        target = reward - np.sum(self.w[self.current_action][self.current_tiles])
-        self.w[self.current_action][self.current_tiles] += self.learning_rate * target
+
+        current_action = self.next_action
+        current_tiles = self.next_tiles
+
+        target = reward - np.sum(self.w[current_action][current_tiles])
+        self.w[current_action][current_tiles] += self.learning_rate * target
 
 
 class ExpectedSarsaAgent(Agent):
 
     def __init__(self, agent_init):
         super().__init__(agent_init)
-        self.current_tiles = None
-        self.current_action = None
+        self.next_tiles = None
+        self.next_action = None
 
-    def choose_action(self, state):
+    def episode_init(self, state):
 
-        if self.current_tiles is None:
-            active_tiles = self.tile_coder.get_active_tiles(state)
-        else:
-            active_tiles = self.current_tiles
-
+        active_tiles = self.tile_coder.get_active_tiles(state)
         action = self.e_greedy(active_tiles)
 
-        if self.current_tiles is None:
-            self.current_tiles = np.copy(active_tiles)
-
-        self.current_action = action
+        self.next_tiles = np.copy(active_tiles)
+        self.next_action = action
 
         return action
 
     def update_step(self, next_state, reward):
 
+        current_action = self.next_action
+        current_tiles = self.next_tiles
+
         next_active_tiles = self.tile_coder.get_active_tiles(next_state)
 
         target = reward + self.discount_factor * np.mean(self.w[:, next_active_tiles].sum(axis=1)) \
-                 - np.sum(self.w[self.current_action][self.current_tiles])
-        self.w[self.current_action][self.current_tiles] += self.learning_rate * target
+                 - np.sum(self.w[current_action][current_tiles])
+        self.w[current_action][current_tiles] += self.learning_rate * target
 
-        self.current_tiles = next_active_tiles
+        self.next_tiles = np.copy(next_active_tiles)
+        self.next_action = self.e_greedy(next_active_tiles)
+
+        return self.next_action
 
     def update_end(self, state, reward):
-        target = reward - np.sum(self.w[self.current_action][self.current_tiles])
-        self.w[self.current_action][self.current_tiles] += self.learning_rate * target
+
+        current_action = self.next_action
+        current_tiles = self.next_tiles
+
+        target = reward - np.sum(self.w[current_action][current_tiles])
+        self.w[current_action][current_tiles] += self.learning_rate * target
 
