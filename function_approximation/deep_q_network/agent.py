f5d29cce8103d577e49f2d04dd0bfddf46d2b6be
¨BenoitLeguay¨ <benoit.leguay@gmail.com>
2020-05-01 16:58:06 +0200

    [DQN] dqn agent done. trainsession object (to perform multiple agent train) done

--- function_approximation/deep_q_network/agent.py
+++ function_approximation/deep_q_network/agent.py
@@ -6,41 +6,28 @@ import utils
 
 class Agent:
     def __init__(self, agent_init):
-        self.discount_factor = agent_init["discount_factor"]
         self.exploration_handler = utils.ExplorationRateDecay(*agent_init["exploration_rate"].values())
         self.num_action = agent_init["num_action"]
-        self.nn_handler = utils.NeuralNetworkHandler(agent_init["nn_archi"], agent_init["learning_rate"])
+        self.nn_handler = utils.NeuralNetworkHandler(agent_init["neural_network_handler"], agent_init['optim'], agent_init['replay_buffer'])
         self.max_position = agent_init["max_position_init"]
         self.max_position_reward_bonus = agent_init["max_position_reward_bonus"]
+        self.random_generator = np.random.RandomState(seed=agent_init['seed'])
         self.next_state = None
         self.next_action = None
         self.next_values = None
 
-    @staticmethod
-    def init_nn(nn_archi):
+    def e_greedy(self, state):
 
-        layers = list()
+        state_tensor = self.nn_handler.to_float_tensor(state)
+        values = self.nn_handler.eval_nn(state_tensor).detach()
 
-        for layer_args in nn_archi:
-            layer_type = layer_args['type']
-            in_features, out_features, activation = layer_args["in"], layer_args["out"], layer_args["activation"]
-            layers.append(v.layer_name_to_obj[layer_type](in_features, out_features))
-            layers.append(v.activation_name_to_obj[activation]())
-
-        dqn = torch.nn.Sequential(*layers)
-        return dqn
-
-    def e_greedy(self, state_tensor):
-
-        values = self.nn_handler.nn(state_tensor).detach()
-
-        if np.random.rand() < self.exploration_handler.rate:
-            action = np.random.randint(self.num_action)
+        if self.random_generator.rand() < self.exploration_handler():
+            action = self.random_generator.randint(self.num_action)
         else:
 
             action = values.max(0)[1].item()
 
-        return action, values
+        return action
 
     def max_position_reward_function(self, new_position, reward):
         if new_position > self.max_position:
@@ -50,20 +37,21 @@ class Agent:
         return reward
 
     def episode_init(self, state):
-        state_tensor = self.nn_handler.state_to_tensor(state)
-        action_tensor, next_values = self.e_greedy(state_tensor)
-        self.next_action = action_tensor
-        self.next_state = state_tensor
-        self.next_values = next_values
+        action = self.e_greedy(state)
+        self.next_action = action
+        self.next_state = state
 
-        return action_tensor
+        return action
 
     def update(self, state, reward, done):
+
+        reward = self.max_position_reward_function(state[0], reward)
 
         next_action = -1
         if not done:
             next_action = self.update_step(state, reward)
         if done:
+            self.exploration_handler.next()
             self.update_end(reward)
 
         return next_action
@@ -76,34 +64,22 @@ class Agent:
 
 
 class DQN(Agent):
-    def update_step(self, next_state, reward):
 
+    def update_step(self, next_state, reward):
         current_action = self.next_action
         current_state = self.next_state
-        current_values = self.next_values
 
-        next_state_tensor = self.nn_handler.state_to_tensor(next_state)
-        next_action, next_values = self.e_greedy(next_state_tensor)
+        self.nn_handler.update_step(current_state, current_action, reward, next_state, 0)
 
-        expected_values = current_values.clone()
+        next_action = self.e_greedy(next_state)
 
-        # if sarsa, take next_values[next_action]; if q learning, take next_values.max(0)[1]
-        expected_values[current_action] = reward + self.discount_factor * next_values.max(0)[0]
-
-        self.nn_handler.update_nn(current_values, expected_values)
-
+        self.next_state = next_state
         self.next_action = next_action
-        self.next_state = next_state_tensor
-        self.next_values = next_values
 
         return next_action
 
     def update_end(self, reward):
         current_action = self.next_action
         current_state = self.next_state
-        current_values = self.next_values
 
-        expected_values = current_values.clone()
-        expected_values[current_action] = reward
-
-        self.nn_handler.update_nn(current_values, expected_values)
+        self.nn_handler.update_step(current_state, current_action, reward, current_state, 1)
--- function_approximation/deep_q_network/utils.py
+++ function_approximation/deep_q_network/utils.py
@@ -1,6 +1,10 @@
 import numpy as np
 import torch
 import variable as v
+from collections import deque, namedtuple
+from tqdm import tqdm
+import random
+import matplotlib.pyplot as plt
 
 
 def moving_average(a, n=3):
@@ -15,18 +19,26 @@ def argmax(a):
 
 
 class NeuralNetworkHandler:
-    def __init__(self, nn_archi, optimizer_args):
-        self.nn = self.init_nn(nn_archi)
+    def __init__(self, nnh_init, optimizer_args={}, replay_buffer_args={}):
+        torch.manual_seed(nnh_init['seed'])
+        self.test = list()
+        self.discount_factor = nnh_init["discount_factor"]
+        self.eval_nn = self.init_nn(nnh_init["nn_archi"])
+        self.target_nn = self.init_nn(nnh_init["nn_archi"])  # deepcopy(self.eval_nn)
+        self.eval_train_delay = nnh_init["eval_train_delay"]
         self.loss = torch.nn.MSELoss()
-        self.optimizer = torch.optim.Adam(self.nn.parameters(), *optimizer_args)
+        self.optimizer = torch.optim.Adam(self.eval_nn.parameters(), **optimizer_args)
+        self.replay_buffer = ReplayBuffer(**replay_buffer_args)
         self.loss_history = list()
+        self.count_step = 0.0
 
     @staticmethod
-    def state_to_tensor(state):
-        return torch.tensor(state).type(torch.FloatTensor)
+    def to_float_tensor(a):
+        return torch.tensor(a).type(torch.FloatTensor)
 
     @staticmethod
     def init_nn(nn_archi):
+
         layers = list()
 
         for layer_args in nn_archi:
@@ -40,15 +52,61 @@ class NeuralNetworkHandler:
         nn = torch.nn.Sequential(*layers)
         return nn
 
+    def update_step(self, current_state, current_action, reward, next_state, done):
+
+        self.replay_buffer.append(current_state, current_action, reward, next_state, done)
+
+        if self.count_step % self.eval_train_delay == 0:
+            self.target_nn.load_state_dict(self.eval_nn.state_dict())
+        self.count_step += 1
+
+        if len(self.replay_buffer) == self.replay_buffer.buffer_size:
+            experiences = self.replay_buffer.sample()
+            self.train(experiences)
+            self.test.append(1.0)
+        else:
+            self.test.append(0.0)
+
+    def train(self, experiences):
+
+        states, actions, rewards, next_states, dones = map(self.to_float_tensor, list(zip(*experiences)))
+
+        current_values = self.eval_nn(states).gather(1, actions.long().unsqueeze(1)).squeeze()
+
+        next_values = self.target_nn(next_states).detach()
+        next_values[dones.bool()] = 0.0
+        expected_values = rewards + self.discount_factor * next_values.max(1)[0]
+
+        self.update_nn(current_values, expected_values)
+
     def update_nn(self, current_values, expected_values):
 
         loss = self.loss(current_values, expected_values)
-        loss.requires_grad = True
+        #  loss.requires_grad = True
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
 
         self.loss_history.append(loss.item())
+
+
+class ReplayBuffer:
+    def __init__(self, buffer_size=1000, mini_batch_size=128, seed=42):
+        random.seed(seed)
+        self.buffer = deque(maxlen=buffer_size)
+        self.experience = namedtuple('Experience', field_names=["state", "action", "reward", "next_state", "done"])
+        self.mini_batch_size = mini_batch_size
+        self.buffer_size = buffer_size
+
+    def __len__(self):
+        return len(self.buffer)
+
+    def append(self, state, action, reward, next_state, done):
+        experience = self.experience(state, action, reward, next_state, done)
+        self.buffer.append(experience)
+
+    def sample(self):
+        return random.choices(list(self.buffer), k=self.mini_batch_size)
 
 
 class ExplorationRateDecay:
@@ -60,6 +118,9 @@ class ExplorationRateDecay:
         self.episode_count = 0.0
         self.constant_er = constant_exploration_rate
         self.history = list()
+
+    def __call__(self):
+        return self.rate
 
     def next(self):
 
@@ -76,3 +137,77 @@ class ExplorationRateDecay:
 
     def reset(self):
         self.episode_count = 0.0
+
+
+class TrainSession:
+    def __init__(self, agents, env, seed):
+        env.seed(seed)
+        self.agents = agents
+        self.env = env
+        self.rewards_per_episode = {agent_name: np.array([]) for agent_name, _ in agents.items()}
+        self.time_steps_per_episode = {agent_name: np.array([]) for agent_name, _ in agents.items()}
+
+    def train(self, n_episode=500, t_max_per_episode=200, graphical=False):
+
+        for agent_name, agent in self.agents.items():
+
+            time_steps_per_episode = list()
+            rewards_per_episode = list()
+
+            for _ in tqdm(range(n_episode)):
+
+                rewards = 0.0
+                state = self.env.reset()
+                next_action = agent.episode_init(state)
+
+                for t in range(t_max_per_episode):
+                    if graphical:
+                        self.env.render()
+
+                    state, reward, done, info = self.env.step(next_action)
+                    next_action = agent.update(state, reward, done)
+
+                    rewards += reward
+
+                    if done:
+                        break
+                time_steps_per_episode.append(t)
+                rewards_per_episode.append(rewards)
+
+            self.time_steps_per_episode[agent_name] = np.concatenate([self.time_steps_per_episode[agent_name],
+                                                                      np.array(time_steps_per_episode)])
+            self.rewards_per_episode[agent_name] = np.concatenate([self.rewards_per_episode[agent_name],
+                                                                   np.array(rewards_per_episode)])
+
+            self.env.close()
+
+    def append_agent(self, agents):
+
+        assert all(item in agents for item in self.agents), "You are trying to overwrite agents dictionary"
+
+        self.agents.update(agents)
+        self.rewards_per_episode.update({agent_name: np.array([]) for agent_name, _ in agents.items()})
+        self.time_steps_per_episode.update({agent_name: np.array([]) for agent_name, _ in agents.items()})
+
+    def plot_results(self, moving_average_n=200):
+        series_to_plot = {'rewards': self.rewards_per_episode,
+                          'time_steps': self.time_steps_per_episode}
+
+        loss_per_agents = {'loss': {agent_name: (np.array(agent.nn_handler.loss_history) if 'nn_handler' in agent.__dict__.keys()
+                                                 else np.array([]))
+                                    for agent_name, agent in self.agents.items()}}
+
+        series_to_plot.update(loss_per_agents)
+
+        for idx, (series_name, dict_series) in enumerate(series_to_plot.items()):
+
+            figure = plt.figure(idx)
+            for agent_name, series in dict_series.items():
+                series_mov_avg = moving_average(series, n=moving_average_n)
+                plt.plot(range(len(series_mov_avg)), series_mov_avg, label=agent_name)
+            figure.suptitle(f"{series_name} per episode", fontsize=15)
+            plt.ylabel(f"avg {series_name}", fontsize=10)
+            plt.xlabel(f"episodes", fontsize=10)
+            plt.legend()
+
+
